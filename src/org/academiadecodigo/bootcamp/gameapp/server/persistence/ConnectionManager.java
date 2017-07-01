ce5c8d7b5c16e04fd5ee88d828c290bcf199b673
Nelson Pereira <nmfp@mail.com>
2017-07-01 15:46:59 +0100

    DB, mysql-connector, Class User, ConnectionManager and JdbcUserService, Interfaces Service and UserService

---
+++ src/org/academiadecodigo/bootcamp/gameapp/server/persistence/ConnectionManager.java
@@ -0,0 +1,42 @@
+package org.academiadecodigo.bootcamp.gameapp.server.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by codecadet on 01/07/2017.
+ */
+public class ConnectionManager {
+
+    Connection connection;
+
+    public Connection getConnection() {
+
+        if (connection == null) {
+
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://192.168.1.18:3306/users", "root", "");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failure to connect to database : " + e.getMessage());
+                System.exit(1);
+            }
+        }
+        return connection;
+    }
+
+    public void close() {
+
+        if (connection != null) {
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failure to close database connections: " + e.getMessage());
+                System.exit(1);
+            }
+        }
+    }
+}
---
+++ src/org/academiadecodigo/bootcamp/gameapp/server/service/Service.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.bootcamp.gameapp.server.service;
+
+/**
+ * A/C: Bootcamp8
+ * 2nd group project - Game App Platform
+ * Authors: Cyrille Feijó, João Fernandes, Hélder Matos, Nelson Pereira, Tiago Santos
+ */
+
+public interface Service {
+
+    String getServiceName();
+}
